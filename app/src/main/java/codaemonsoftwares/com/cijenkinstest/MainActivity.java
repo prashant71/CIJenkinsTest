c3f3ff321f8fc83fd11a098eb57fa5b5e606de6c
prashant koli <prashant.koli@codaemonsoftwares.com>
2017-09-20 18:16:56 +0530

    comment added

--- app/src/main/java/codaemonsoftwares/com/cijenkinstest/MainActivity.java
+++ app/src/main/java/codaemonsoftwares/com/cijenkinstest/MainActivity.java
@@ -2,7 +2,7 @@ package codaemonsoftwares.com.cijenkinstest;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-
+/*Hello this is Android Workspace for CI jenkins Test*/
 public class MainActivity extends AppCompatActivity {
 
     @Override
