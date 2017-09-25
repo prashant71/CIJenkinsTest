2c07675aa74bcc280e0841604d9d48c6178b8e4d
prashant koli <prashant.koli@codaemonsoftwares.com>
2017-09-25 17:08:52 +0530

    Github Plugin addition steps write in comment section

--- app/src/main/java/codaemonsoftwares/com/cijenkinstest/MainActivity.java
+++ app/src/main/java/codaemonsoftwares/com/cijenkinstest/MainActivity.java
@@ -3,6 +3,10 @@ package codaemonsoftwares.com.cijenkinstest;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 /*Hello this is Android Workspace for CI jenkins Test*/
+/*to trigger Build automatically on Jenkins when code is commited on Github repo need to follow below steps :
+* 1.Install Plugin on jenkins - github integration plugin
+* 2.Goto Project and configure >> Build Trigger >> check GitHub hook trigger for GITScm polling and save
+* 3.Goto Github >> setting >> webhook >> add webhook >> add jenkins project URL and save.*/
 public class MainActivity extends AppCompatActivity {
 
     @Override
